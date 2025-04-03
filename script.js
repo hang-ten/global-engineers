35d48228a6edd52d6b89fd848a3d2c8e94b8f8a7
hang-ten <138254527+hang-ten@users.noreply.github.com>
2025-04-03 10:11:22 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+/** HOME PAGE */
+
+function toggleMenu() {
+    const menu = document.getElementById('circular-menu');
+    menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'flex' : 'none';
+}
